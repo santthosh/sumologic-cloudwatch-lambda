7428ecfb938ed49b5d0c25a4b02bc4d9f4391662
Santthosh <santthosh@gmail.com>
2015-12-26 17:54:14 +0530

    Basic functionality

--- index.js
+++ index.js
@@ -5,6 +5,9 @@
  */
 
 var zlib = require('zlib');
+var config = require('config.json');
+var request = require('request');
+
 exports.handler = function(event, context) {
     var payload = new Buffer(event.awslogs.data, 'base64');
     zlib.gunzip(payload, function(e, result) {
@@ -12,8 +15,34 @@ exports.handler = function(event, context) {
             context.fail(e);
         } else {
             result = JSON.parse(result.toString('utf8'));
-            console.log("Decoded payload: ", JSON.stringify(result));
-            context.succeed("Successfully processed " + result.logEvents.length + " log events.");
+            var lines = "";
+            result.logEvents.forEach(function(log) {
+                lines += log.timestamp + " name=" + result.logGroup + " stream="+ result.logStream + " message=" + log.message.replace(/\n$/g,'') + "\n";
+            });
+            console.log(lines);
+            zlib.gzip(lines, function(err, zipped) {
+                if (!err) {
+                    request(
+                        {
+                            method: 'POST',
+                            uri: config.sumologic.collector.url,
+                            headers: {
+                                'Content-Encoding': 'gzip',
+                                'X-Sumo-Name': result.logGroup
+                            },
+                            body: zipped
+                        }, function (error, response, body) {
+                            if(response.statusCode == 200){
+                                context.succeed("Successfully processed " + result.logEvents.length + " log events.");
+                            } else {
+                                context.fail(error);
+                            }
+                        }
+                    )
+                } else {
+                    context.fail(err);
+                }
+            });
         }
     });
 };
